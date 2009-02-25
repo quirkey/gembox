de1720ea1f42bfa1db151474f4c28d84b2695966
Aaron Quint <aaron@quirkey.com>
2009-02-25 12:31:10 -0500

    Fixing test failures

--- test/test_gembox_app.rb
+++ test/test_gembox_app.rb
@@ -1,68 +1,68 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 describe "Gembox App" do
-  
+
   describe 'getting /' do
     before do
       get '/'
     end
-    
+
     should 'load the index' do  
       should.be.ok
     end
-    
+
     should "display gem list" do
       body.should have_element('#gems')
     end
-    
+
     should "display list of installed gems" do
       body.should have_element('.gem', /sinatra/)
     end
-    
+
     should "display as 4 columns" do
       body.should have_element('.column')
     end
   end
-  
+
   describe 'getting gems/ with layout = false' do
     before do
       get '/gems/?layout=false'
     end
-    
+
     should "load" do
       should.be.ok
     end
-    
+
     should "not display layout" do
       body.should.not have_element('html')
     end
-    
+
     should "display gem list" do
       body.should have_element('#gems')
     end
-    
+
     should "display as 4 columns" do
       body.should have_element('.column')
     end
   end
-  
+
   describe 'getting gems/ with a simple search' do
     before do
       get '/gems/?search=s'
     end
-    
+
     should "load" do
       should.be.ok
     end
-    
+
     should "display gem list" do
       body.should have_element('#gems')
     end
-    
+
     should "display gems that match the search" do
       body.should have_element('.gem', /sinatra/)
     end
-    
+
     should "not display gems that do not match the search" do
       body.should.not have_element('.gem', /rack/)
     end
@@ -72,30 +72,30 @@ describe "Gembox App" do
     before do
       get '/gems/?layout=false&as=table'
     end
-    
+
     should "load" do
       should.be.ok
     end
-    
+
     should "not display layout" do
       body.should.not have_element('html')
     end
-    
+
     should "display gem list" do
       body.should have_element('#gems')
     end
-    
-    should "display as table"
+
+    should "display as table" do
       body.should have_element('table#gems')
       body.should have_element('table#gems tr.gem')
     end
   end
-  
+
   describe 'getting gems/:name' do
     before do
       get '/gems/sinatra'
     end
-    
+
     should "display only specific gem" do
       body.should.not have_element('.gem', /rack/)
     end
@@ -103,21 +103,21 @@ describe "Gembox App" do
     should "display link to gems website" do
       body.should have_element('a', 'http://sinatra.rubyforge.org')
     end
-    
+
     should "load gem spec for latest version" do
       body.should have_element('.version', '0.9.0.5')
     end
-    
+
     should "display links to all versions" do
       body.should have_element('.other_versions a[href="/gems/sinatra/0.9.0.2"]')
     end
   end
-  
+
   describe 'getting gems/name/version' do
     before do
       get '/gems/sinatra/0.9.0.2'
     end
-    
+
     should "display only specific gem" do
       body.should.not have_element('.gem', /rack/)
     end
@@ -125,14 +125,14 @@ describe "Gembox App" do
     should "display link to gems website" do
       body.should have_element('a', 'http://sinatra.rubyforge.org')
     end
-    
+
     should "load gem spec specified version" do
       body.should have_element('.version', '0.9.0.2')
     end
-    
+
     should "display links to all versions" do
       body.should have_element('.other_versions a[href="/gems/sinatra/0.9.0.5"]')
     end
   end
-  
+
 end
